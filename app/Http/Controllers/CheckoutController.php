858d318f7950436a4c40462c866e5eb3b0b20bf1
Dipak Dhola <dhola.dipak09@gmail.com>
2024-07-20 22:39:23 +0530

    make more validation in checkout and save stripe card details in DB

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -6,6 +6,8 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Stripe\Stripe;
 use Stripe\Charge;
+use Stripe\PaymentIntent;
+use Stripe\PaymentMethod;
 use Stripe\Exception\CardException;
 use Alert;
 
@@ -14,21 +16,48 @@ class CheckoutController extends Controller
     public function checkout(Request $request, Product $product)
     {
         Stripe::setApiKey(env('STRIPE_SECRET'));
+        $user = auth()->user();
+        $paymentAmount = $product->price * 100;
+
+        // Create a new Stripe customer if not already created
+        if (!$user->stripe_id) {
+            $user->createAsStripeCustomer();
+        }
 
         try {
-            $charge = Charge::create([
-                'amount' => $product->price * 100,
-                'currency' => 'usd',
-                'description' => $product->name,
-                'receipt_email' => auth()->user()->email,
-                'source' => $request->stripeToken,
-            ]);
+            $paymentMethodId = $request->payment_method_id;
+            $paymentMethod = PaymentMethod::retrieve($paymentMethodId);
+            $paymentMethod->attach(['customer' => $user->stripe_id]);
 
-            // Handle successful charge here
+            try {
+                
+                // Create and confirm a Payment Intent with a return URL (if needed)
+                $paymentIntent = \Stripe\PaymentIntent::create([
+                    'amount' => $paymentAmount,
+                    'currency' => 'usd',
+                    'payment_method' => $paymentMethodId,
+                    'description' => $product->name,
+                    'receipt_email' => $user->email,
+                    'customer' => $user->stripe_id,
+                    'confirmation_method' => 'automatic',
+                    'confirm' => true,
+                    'return_url' => route('home'), // Adjust this route to your needs
+                ]);
+
+                // Check the status of the PaymentIntent
+                if ($paymentIntent->status != 'succeeded') {
+                    Alert::error('Payment Failed', "Please try again");
+                    return redirect('/home');
+                }
+            } catch (\Throwable $th) {
+                \Log::error($th);
+                Alert::error('Payment Failed', $th->getMessage());
+                return redirect('/home');
+            }
             Alert::success('Payment Successful', 'Thank you for your purchase!');
             return redirect('/home');
         } catch (CardException $e) {
-            // Handle card errors
+            \Log::error($e);
             Alert::error('Payment Failed', $e->getMessage());
             return back();
         }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -19,7 +19,8 @@
                             </div>
                             <div id="card-errors" role="alert"></div>
                         </div>
-                        <button class="btn btn-primary">Submit Payment</button>
+                        <button id="btnSubmit" class="btn btn-primary">Submit Payment</button>
+                        <a id="btnBack" class="btn btn-secondary" href="{{ url('/home') }}">Back</a>
                     </form>
                 </div>
             </div>
@@ -29,60 +30,72 @@
 
 <script src="https://js.stripe.com/v3/"></script>
 <script>
-var stripe = Stripe('{{ env('STRIPE_KEY') }}');
-var elements = stripe.elements();
-
-var style = {
-    base: {
-        color: '#32325d',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
+    var stripe = Stripe('{{ env('STRIPE_KEY') }}');
+    var elements = stripe.elements();
+    
+    var style = {
+        base: {
+            color: '#32325d',
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+                color: '#aab7c4'
+            }
+        },
+        invalid: {
+            color: '#fa755a',
+            iconColor: '#fa755a'
         }
-    },
-    invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a'
-    }
-};
-
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
-
-card.addEventListener('change', function(event) {
-    var displayError = document.getElementById('card-errors');
-    if (event.error) {
-        displayError.textContent = event.error.message;
-    } else {
-        displayError.textContent = '';
-    }
-});
-
-var form = document.getElementById('payment-form');
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    stripe.createToken(card).then(function(result) {
-        if (result.error) {
-            var errorElement = document.getElementById('card-errors');
-            errorElement.textContent = result.error.message;
+    };
+    
+    var card = elements.create('card', {style: style});
+    card.mount('#card-element');
+    
+    card.addEventListener('change', function(event) {
+        var displayError = document.getElementById('card-errors');
+        if (event.error) {
+            displayError.textContent = event.error.message;
         } else {
-            stripeTokenHandler(result.token);
+            displayError.textContent = '';
         }
     });
-});
-
-function stripeTokenHandler(token) {
+    
     var form = document.getElementById('payment-form');
-    var hiddenInput = document.createElement('input');
-    hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', 'stripeToken');
-    hiddenInput.setAttribute('value', token.id);
-    form.appendChild(hiddenInput);
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        stripe.createPaymentMethod({
+            type: 'card',
+            card: card,
+            billing_details: {
+                name: '{{ auth()->user()->name }}',
+                email: '{{ auth()->user()->email }}'
+            },
+        }).then(function(result) {
+            if (result.error) {
+                var errorElement = document.getElementById('card-errors');
+                errorElement.textContent = result.error.message;
+            } else {
+                stripePaymentMethodHandler(result.paymentMethod.id);
+            }
+        });
+    });
+    
+    function stripePaymentMethodHandler(paymentMethodId) {
+        var form = document.getElementById('payment-form');
+        var hiddenInput = document.createElement('input');
+        hiddenInput.setAttribute('type', 'hidden');
+        hiddenInput.setAttribute('name', 'payment_method_id');
+        hiddenInput.setAttribute('value', paymentMethodId);
+        form.appendChild(hiddenInput);
+        
+        var btnSubmitEleman = document.getElementById('btnSubmit');
+        btnSubmitEleman.setAttribute("disabled", true);
+        setTimeout(function () { btnSubmitEleman.setAttribute("disabled", false); }, 5000);
 
-    form.submit();
-}
-</script>
+        form.submit();
+
+    }
+    </script>
 @endsection
